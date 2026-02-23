0c9bd01d33f850085220d489c7c6a8062360df11
Halffd <guhalfeld@hotmail.com>
2026-02-23 10:11:05 -0300

    first commit

---
+++ build/CMakeFiles/wayland-compositor.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/half-arch/repos/havel/src/wm/compositor.cpp" "CMakeFiles/wayland-compositor.dir/compositor.cpp.o" "gcc" "CMakeFiles/wayland-compositor.dir/compositor.cpp.o.d"
+  "" "bin/wayland-compositor" "gcc" "CMakeFiles/wayland-compositor.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/wayland-compositor.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wayland-compositor.dir/link.d"
+  "CMakeFiles/wayland-compositor.dir/compositor.cpp.o"
+  "CMakeFiles/wayland-compositor.dir/compositor.cpp.o.d"
+  "bin/wayland-compositor"
+  "bin/wayland-compositor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wayland-compositor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
